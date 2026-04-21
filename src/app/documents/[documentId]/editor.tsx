925c5c5efdc05f7c10ed5f278b73fc6bd4d8aedf
João Gabriel <joaogabriel9633@gmail.com>
2026-04-20 22:03:39 -0300

    Edit tools

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -7,9 +7,36 @@ import { Dropcursor } from "@tiptap/extensions";
 import Image from "@tiptap/extension-image";
 import FileHandler from "@tiptap/extension-file-handler";
 import StarterKit from "@tiptap/starter-kit";
+import { useEditorStore } from "@/store/use-editor-store";
 
 export const Editor = () => {
+  const { setEditor } = useEditorStore();
+
   const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate: ({ editor }) => {
+      setEditor(editor);
+    },
+    onSelectionUpdate: ({ editor }) => {
+      setEditor(editor);
+    },
+    onTransaction: ({ editor }) => {
+      setEditor(editor);
+    },
+    onFocus: ({ editor }) => {
+      setEditor(editor);
+    },
+    onBlur: ({ editor }) => {
+      setEditor(editor);
+    },
+    onContentError: ({ editor }) => {
+      setEditor(editor);
+    },
     editorProps: {
       attributes: {
         style: "padding-left: 56px; padding-right: 56px;",
@@ -85,7 +112,6 @@ export const Editor = () => {
     content: `
         <p>Hello, world</p>
         <img src="https://placehold.co/600x400" />
-       
       `,
     // Don't render immediately on the server to avoid SSR issues
     immediatelyRender: false,
